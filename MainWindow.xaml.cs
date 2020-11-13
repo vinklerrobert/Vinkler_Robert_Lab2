9fbc3f8b1fed1a8b7618e498b424be164b172802
ROBERT VINKLER <robert.vinkler@stud.ubbcluj.ro>
2020-11-13 17:22:27 +0200

    Adaugare functionalitate Lab3

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,13 +20,142 @@ namespace Vinkler_Robert_Lab2
     /// </summary>
     public partial class MainWindow : Window
     {
+        private int mRaisedGlazed = 0;
+        private int mRaisedSugar = 0;
+        private int mFilledLemon = 0;
+        private int mFilledChocolate = 0;
+        private int mFilledVanilla = 0;
+        private double Total = 0;
+        private DoughnutType selectedDoughnut;
+        KeyValuePair<DoughnutType, double>[] PriceList = {
+ new KeyValuePair<DoughnutType, double>(DoughnutType.Sugar, 2.5),
+ new KeyValuePair<DoughnutType, double>(DoughnutType.Glazed,3),
+ new KeyValuePair<DoughnutType, double>(DoughnutType.Chocolate,4.5),
+ new KeyValuePair<DoughnutType, double>(DoughnutType.Vanilla,4),
+ new KeyValuePair<DoughnutType, double>(DoughnutType.Lemon,3.5)
+ };
         public MainWindow()
         {
             InitializeComponent();
         }
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
 
+        }
+
+        private void glazedMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            mRaisedGlazed++;
+            txtGlazedRaised.Text = mRaisedGlazed.ToString();
+
+        }
+
+        private void sugarMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            mRaisedSugar++;
+            txtSugarRaised.Text = mRaisedSugar.ToString();
+
+        }
+
+        private void lemonMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            mFilledLemon++;
+            txtLemonFilled.Text = mFilledLemon.ToString();
+
+        }
+
+        private void chocolateMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            mFilledLemon++;
+            txtLemonFilled.Text = mFilledLemon.ToString();
+        }
+
+        private void vanillaMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            mFilledVanilla++;
+            txtVanillaFilled.Text = mFilledVanilla.ToString();
+        }
+        private void FilledItemsShow_Click(object sender, RoutedEventArgs e)
+        {
+            string mesaj;
+            MenuItem SelectedItem = (MenuItem)e.OriginalSource;
+            mesaj = SelectedItem.Header.ToString() + " doughnuts are being cooked!";
+            this.Title = mesaj;
+        }
+            private void exitMenuItem_Click(object sender, RoutedEventArgs e)
+            {
+                this.Close();
+            }
+        private void frmMain_Loaded(object sender, RoutedEventArgs e)
+        {
+            cmbType.ItemsSource = PriceList;
+            cmbType.DisplayMemberPath = "Key";
+            cmbType.SelectedValuePath = "Value";
+        }
         private void cmbType_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
+            txtPrice.Text = cmbType.SelectedValue.ToString();
+            KeyValuePair<DoughnutType, double> selectedEntry =
+           (KeyValuePair<DoughnutType, double>)cmbType.SelectedItem;
+            selectedDoughnut = selectedEntry.Key;
+        }
+        private int ValidateQuantity(DoughnutType selectedDoughnut)
+        {
+            int q = int.Parse(txtQuantity.Text);
+            int r = 1;
+
+            switch (selectedDoughnut)
+            {
+                case DoughnutType.Glazed:
+                    if (q > mRaisedGlazed)
+                        r = 0;
+                    break;
+                case DoughnutType.Sugar:
+                    if (q > mRaisedSugar)
+                        r = 0;
+                    break;
+                case DoughnutType.Chocolate:
+                    if (q > mFilledChocolate)
+                        r = 0;
+                    break;
+                case DoughnutType.Lemon:
+                    if (q > mFilledLemon)
+                        r = 0;
+                    break;
+                case DoughnutType.Vanilla:
+                    if (q > mFilledVanilla)
+                        r = 0;
+                    break;
+            }
+            return r;
+        }
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            if (ValidateQuantity(selectedDoughnut) > 0)
+            {
+                lstSale.Items.Add(txtQuantity.Text + " " + selectedDoughnut.ToString() +
+               ":" + txtPrice.Text + " " + double.Parse(txtQuantity.Text) *
+               double.Parse(txtPrice.Text));
+                Total = Total + double.Parse(txtQuantity.Text) * double.Parse(txtPrice.Text);
+                txtTotal.Text = Total.ToString();
+            }
+            else
+            {
+                MessageBox.Show("Cantitatea introdusa nu este disponibila in stoc!");
+            }
+        }
+        private void btnRemoveItem_Click(object sender, RoutedEventArgs e)
+        {
+            lstSale.Items.Remove(lstSale.SelectedItem);
+        }
+
+        private void txtQuantity_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (!(e.Key >= Key.D0 && e.Key <= Key.D9))
+            {
+                MessageBox.Show("Numai cifre se pot introduce!", "Input Error", MessageBoxButton.OK,
+               MessageBoxImage.Error);
+            }
 
         }
     }
